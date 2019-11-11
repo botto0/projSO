5211d76f09b178e4ce1687929d8ba7fef9562c1f
Lucas Sousa <botto@MacBook-Pro-de-Lucas.local>
2019-11-11 23:20:50 +0000

    ..

--- userstats.sh
+++ userstats.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
-while getopts "ab" arg; do
-    case $arg in
-    "a") Name=$arg ;;
-    "b") Name=$arg ;;
+declare -a info_total
+declare -a info_filtrada
 
-    esac
-    echo "argumento : $Name!"
+info_filtrada[0] =lucas
+
+for i in "${info_filtrada[@]}"
+do
+echo $i
 done
+# if [ "$#" -eq 0 ]; then
+#     echo "teste"
+# fi
+# if (("$#" > 0)); then
+#     while getopts "gusefrntai" arg; do
+#         case $arg in
+#         "g")
+#             last | awk '{print $1, user++ }' | sort -
+#             Name=$arg
+#             ;;
+#         "u") Name=$arg ;;
+#         "s") Name=$arg ;;
+#         "e") Name=$arg ;;
+#         "f") Name=$arg ;;
+#         "r") Name=$arg ;;
+#         "n") Name=$arg ;;
+#         "t") Name=$arg ;;
+#         "a") Name=$arg ;;
+#         "i") Name=$arg ;;
+#         esac
+#         echo "argumento : $Name"
+#     done
+# fi
