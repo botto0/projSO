80a4f767fcaddf37695fd086ff84ab31090a3514
Miguel <almeida.m@ua.pt>
2019-11-14 12:50:44 +0000

    update

--- userstats.sh
+++ userstats.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/bash
+#!/bin/bash
 
 #output: nome_utilizador numero_sessões tempo_total duraçao_max duraçao_min
 #tempo vem em minutos
@@ -9,9 +9,9 @@
 
 # grep "\S"-remove todas as linhas em branco
 
-users=$(last | awk '{print $1}' | grep "\S")
+users=$(last $USERS | column -t| awk '{print $1}' | grep "\S")
 # numberUsers=$(last|awk "{print $1}| wc- w")
-
+# grep -v "string" - procura pelas linhas que não contem a string
 
 # declarar um array associativo
 declare -A user
@@ -25,23 +25,47 @@ for i in $users; do
 done
 
 for x in "${!user[@]}"; do
-    echo "${x}" #k
-    echo "${user[$x]}" #v
+    printf "%s  %s\n" $x ${user[$x]}
 done
 
 echo "-------------------------------------------------------------"
 
+# Para obter os campos de tempo atraves do awk não se consegue para os casos do rebbot e wtmp pois os campos estão decalibrados
+#relativamente ao aos utlizadores 
+
+# miguel  :0      Wed   Nov  13   23:14     still  logged  in
+# reboot  system  boot  Wed  Nov  13        23:14  still   running
+# miguel  :0      Tue   Nov  12   09:20     -      14:45   (05:24)
+# reboot  system  boot  Tue  Nov  12        09:20  -       14:45    (05:24)
+# miguel  :0      Mon   Nov  11   12:25     -      21:23   (08:58)
+# reboot  system  boot  Mon  Nov  11        12:24  -       21:23    (08:58)
+# miguel  :0      Sun   Nov  10   10:55     -      down    (22:18)
+# reboot  system  boot  Sun  Nov  10        10:55  -       09:14    (22:18)
+# miguel  :0      Sun   Nov  10   10:51     -      10:55   (00:03)
+# reboot  system  boot  Sun  Nov  10        10:51  -       10:55    (00:04)
+# miguel  :0      Sun   Nov  10   10:44     -      crash   (00:06)
+# reboot  system  boot  Sun  Nov  10        10:44  -       10:55    (00:10)
+# miguel  tty1    Sun   Nov  10   10:18     -      10:44   (00:25)
+# reboot  system  boot  Sun  Nov  10        10:18  -       10:44    (00:25)
+# miguel  tty1    Sun   Nov  10   10:00     -      10:16   (00:16)
+# reboot  system  boot  Sun  Nov  10        10:00  -       10:18    (00:18)
+# wtmp    begins  Sun   Nov  10   09:59:52  2019
+
+
 declare -A user_time
 
-users_times=$(last | awk '{print $9}' | grep "\S")
+#users_times=$(last | awk '{print $9}' | grep "\S")
 
-for i in $users_times; do 
-    if [[ $i != "in" &&  -z user_time ]] ; then # $i != "in" nao esta a retirar dos tempos o 'in' nao sei porque 
-        user_time[$i] = 1
-    else 
-        let "user_time[$i]++"
-    fi
-done
+LAST=$(last)
+
+# for x in "${!user[@]}"
+# do
+#     u=$(grep "$x")
+#     if [ "$u" == "reboot "];then
+#     users_time[$x]=$(last| grep "$u" | awk '{for(i=5;i<11;i++) print }' 
+
+
+#     unset u
 
 for i in "${!user_time[@]}"; do
     echo "${i}"
